8dea8c6166af3438c98bb54d93dda8fe3534c036
Danh <danhB2111917@studen.ctu.edu.vn>
2024-09-11 22:10:10 +0700

    Serve static files

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const {
   resourceNotFound,
   handleError,
 } = require("./controllers/errors.controller");
+const { specs, swaggerUi } = require("./docs/swagger");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   return res.json(JSend.success());
 });
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/public", express.static("public"));
 
 contactsRouter.setup(app);
