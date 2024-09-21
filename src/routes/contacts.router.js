b1835b31f6c856cd8c54993646c2372408d40ab2
Danh <danhB2111917@studen.ctu.edu.vn>
2024-09-21 20:46:45 +0700

    Implement handlers

--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const contactsController = require("../controllers/contacts.controller");
 const { methodNotAllowed } = require("../controllers/errors.controller");
+const avatarUpload = require("../middlewares/avatar-upload.middleware");
+
 const router = express.Router();
 
 module.exports.setup = (app) => {
@@ -23,6 +25,8 @@ module.exports.setup = (app) => {
    *         schema:
    *           type: string
    *         description: Filter by contact name
+   *       - $ref: '#/components/parameters/limitParam'
+   *       - $ref: '#/components/parameters/pageParam'
    *     tags:
    *       - contacts
    *     responses:
@@ -44,6 +48,8 @@ module.exports.setup = (app) => {
    *                       type: array
    *                       items:
    *                         $ref: '#/components/schemas/Contact'
+   *                     metadata:
+   *                       $ref: '#/components/schemas/PaginationMetadata'
    */
   router.get("/", contactsController.getContactsByFilter);
   /**
@@ -78,7 +84,7 @@ module.exports.setup = (app) => {
    *                     contact:
    *                       $ref: '#/components/schemas/Contact'
    */
-  router.post("/", contactsController.createContact);
+  router.post("/", avatarUpload, contactsController.createContact);
   /**
    * @swagger
    *  /api/v1/contacts:
@@ -159,7 +165,7 @@ module.exports.setup = (app) => {
    *                     contact:
    *                       $ref: '#/components/schemas/Contact'
    */
-  router.put("/:id", contactsController.updateContact);
+  router.put("/:id", avatarUpload, contactsController.updateContact);
 
   /**
    * @swagger
